c0e09c7c9612e2057fee0eadfe87bc81ca6cf782
SungHyun Yu <ysh1513@naver.com>
2019-12-01 11:02:25 +0900

    파일선택칸&pug

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/multer', function(req, res, next) {
+  res.render('multer-list', { title: 'Express' });
 });
 
 module.exports = router;
